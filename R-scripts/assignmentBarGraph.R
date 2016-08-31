42c27a66ec1ee0ca75b6bcf4736e1257760269ea
supahchadu <nuguchaka@gmail.com>
2016-08-30 23:48:54 -0700

    updated for vertical graph bar in the Y axis

--- R-scripts/assignmentBarGraph.R
+++ R-scripts/assignmentBarGraph.R
@@ -4,17 +4,17 @@ raw<-read.csv("SampleData.csv", sep=",") #Reads the data in each comma
 mm<- melt(raw,id="Person") #Formats the data frame to rely on Person as Y axis
 
 # for switching the order of variables present in the plot
-mm$Person<-factor(mm$Person, levels=c("Experiment","Control"))
+#mm$Person<-factor(mm$Person, levels=c("Experimental","Control"))
 # pass in our Data Frame var mm and feed which data to graph
-p<- ggplot(mm, aes(x=Person,y=value, group=variable)) 
-p<- p + geom_bar(width=.3,aes(fill=variable),stat = "identity", position="dodge")
+p<- ggplot(mm, aes(x=Person,y=value, group=variable), width=15) 
+p<- p + geom_bar(width=.7,aes(fill=variable),stat = "identity", position="dodge")
 p<- p + scale_fill_manual("legend", labels=c("Female", "Male"),values = c("G1" = "#f9cb9c", "G2" = "#3d85c6"))
 p<- p + labs(title="")
 p<- p + xlab("")
 p<- p + ylab("Confidence in Understanding Course Materials")
 p<- p + theme(panel.background=element_blank(), axis.line.x = element_line(color = "black"), axis.line.y = element_line(color="black"), text=element_text(size=25, family = "Times"))
-p<- p + theme(legend.title=element_blank(), legend.background=element_rect(color = "black"), axis.text.x = element_text(face = "bold", margin = margin(5,0,0,0)), axis.title.x=element_text(margin = margin(30,0,0,0)))
+p<- p + theme(legend.title=element_blank(), legend.background=element_rect(color = "black"), legend.margin=unit(1,"cm"),axis.text.x = element_text(face = "bold", margin = margin(2.5,0,0,0)), axis.title.x=element_text(margin = margin(15,0,0,0)))
 p<- p + scale_y_continuous(limit=c(0,5), expand = c(0,0))
 p <- p + coord_flip()
 p
-ggsave(filename = "assignment.pdf", plot = last_plot(), width=210,height=297,units="mm")
+ggsave(filename = "assignment.pdf", plot = last_plot(), width=297,height=105,units="mm")
